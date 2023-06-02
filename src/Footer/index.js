516b2215577eba1dd346c13c21e0e98de0966536
Vũ Tiến Đức <100019107+ducvu0sdsd@users.noreply.github.com>
2023-06-02 18:55:21 +0700

    upload

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -24,7 +24,7 @@ function Footer() {
                     <div className="col-lg-2 col-3  chooseFooter">
                         <p>Family Games</p>
                     </div>
-                    <div className="col-lg-12" style={{display:'flex', justifyContent : 'center',marginBottom : '20px'}}>
+                    <div className="col-lg-12 col-11" style={{display:'flex', justifyContent : 'center',marginBottom : '20px'}}>
                         <div className="col-lg-8 col-7 aboutMe">
                             IC GAMING DOWNLOAD Là Wed Site Tải Game Link Google Drive Miễn Phí, Tốt Độ Cao Bao Gồm : Game Gốc , Game MODS , Game Việt Hóa Nhầm Giúp Cho Người Chơi Có Trải Nghiệm Tốt Nhất Khi Chơi
                         </div>

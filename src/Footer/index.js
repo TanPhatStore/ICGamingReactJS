81c10aa86315014aa7094d1c2e7fb795ba3b5f37
Vũ Tiến Đức <100019107+ducvu0sdsd@users.noreply.github.com>
2023-06-02 18:52:40 +0700

    upload

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -8,36 +8,36 @@ function Footer() {
 
     return ( 
         <div className="wrapperFooter">
-            <div className="logo col-lg-1">
-                <img src={logo} height="100%" />
-            </div>
-            <div className="footer col-lg-7">
-                <div className="col-lg-2 chooseFooter">
-                    <p>Games Library</p>
+                <div className="logo col-lg-1 col-1">
+                    <img src={logo} height="100%" />
                 </div>
-                <div className="col-lg-2 chooseFooter">
-                    <p>Action Games</p>
-                </div>
-                <div className="col-lg-2 chooseFooter">
-                    <p>Fighting Games</p>
-                </div>
-                <div className="col-lg-2 chooseFooter">
-                    <p>Family Games</p>
-                </div>
-                <div className="col-lg-12" style={{display:'flex', justifyContent : 'center',marginBottom : '20px'}}>
-                    <div className="col-lg-8 aboutMe">
-                        IC GAMING DOWNLOAD Là Wed Site Tải Game Link Google Drive Miễn Phí, Tốt Độ Cao Bao Gồm : Game Gốc , Game MODS , Game Việt Hóa Nhầm Giúp Cho Người Chơi Có Trải Nghiệm Tốt Nhất Khi Chơi
+                <div className="footer col-lg-7 col-12">
+                    <div className="col-lg-2 col-3 chooseFooter">
+                        <p>Games Library</p>
                     </div>
-                    <div  className="col-lg-4 contact" >
-                        <div className="col-lg-12 icon">
-                            <i className='bx bxl-youtube'></i> <i className='bx bxl-instagram-alt' ></i> <i className='bx bxl-facebook-circle' ></i>
+                    <div className="col-lg-2 col-3 chooseFooter">
+                        <p>Action Games</p>
+                    </div>
+                    <div className="col-lg-2 col-3 chooseFooter">
+                        <p>Fighting Games</p>
+                    </div>
+                    <div className="col-lg-2 col-3  chooseFooter">
+                        <p>Family Games</p>
+                    </div>
+                    <div className="col-lg-12" style={{display:'flex', justifyContent : 'center',marginBottom : '20px'}}>
+                        <div className="col-lg-8 col-7 aboutMe">
+                            IC GAMING DOWNLOAD Là Wed Site Tải Game Link Google Drive Miễn Phí, Tốt Độ Cao Bao Gồm : Game Gốc , Game MODS , Game Việt Hóa Nhầm Giúp Cho Người Chơi Có Trải Nghiệm Tốt Nhất Khi Chơi
                         </div>
-                        <div className="col-lg-12 copyRight">
-                            © 2023 IC Gaming Inc.
+                        <div  className="col-lg-4 col-4 contact" >
+                            <div className="col-lg-12 icon">
+                                <i className='bx bxl-youtube'></i> <i className='bx bxl-instagram-alt' ></i> <i className='bx bxl-facebook-circle' ></i>
+                            </div>
+                            <div className="col-lg-12 copyRight">
+                                © 2023 IC Gaming Inc.
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
         </div>
      );
 }

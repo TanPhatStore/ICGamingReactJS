5350b7879262c0d99f0306d3fbe97276769cec1d
Vũ Tiến Đức <107258145+TanPhatStore@users.noreply.github.com>
2023-07-07 12:47:41 +0700

    upload

--- src/ListGamesPage/ListGames/index.js
+++ src/ListGamesPage/ListGames/index.js
@@ -8,11 +8,11 @@ import {Context} from '../../UseContext/Context'
 function ListGames({type, series}) {
 
     const [handle, data] = useContext(Context)
-    const games = []
+    const games1 = []
     if (series == 'all-games') {
         data.games.forEach((game , index) => {
             if (game.gameType == type) {
-                games.push(game)
+                games1.push(game)
             }
             
         })
@@ -20,11 +20,29 @@ function ListGames({type, series}) {
         data.games.forEach((game , index) => {
             if(game.series.toLowerCase().split(' ').join('-') == series) {
                 if (game.gameType == type) {
-                    games.push(game)
+                    games1.push(game)
                 }
             }
         })
     }
+    const shuffleArray = (array) => {
+        const shuffledArray = [...array];
+        let currentIndex = shuffledArray.length;
+      
+        while (currentIndex > 0) {
+          const randomIndex = Math.floor(Math.random() * currentIndex);
+      
+          currentIndex--;
+      
+          const temporaryValue = shuffledArray[currentIndex];
+          shuffledArray[currentIndex] = shuffledArray[randomIndex];
+          shuffledArray[randomIndex] = temporaryValue;
+        }
+      
+        return shuffledArray;
+    }
+
+    const games = shuffleArray(games1) 
 
     return ( 
         <div className="col-lg-12 wrapperListGames" style={{display:'flex', justifyContent : 'center'}}>
